88fa6c2c994be568bf702ee98fc18fe3aa2b4226
llSeeSharpll <79083381+llSeeSharpll@users.noreply.github.com>
2022-09-27 13:26:13 +0300

    Full files

---
+++ frontend/src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import data from './data';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Elite Gaming</a>
+      </header>
+      <main>
+        <h1>Feutured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <strong>
+                  <p>${product.price}</p>
+                </strong>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/data.js
@@ -0,0 +1,27 @@
+const data = {
+  products: [
+    {
+      name: 'skyrim specail edition',
+      slug: 'skyrim-specail-edition',
+      category: 'Bethesdagames',
+      image: '/images/skyrimse.jpg',
+      price: 29.99,
+      countInStock: 10,
+      rating: 4.6,
+      numReviews: 10,
+      desc: 'Skyrim specail edition for steam',
+    },
+    {
+      name: 'skyrim specail edition',
+      slug: 'skyrim-specail-edition',
+      category: 'Bethesdagames',
+      image: '/images/skyrimse.jpg',
+      price: 29.99,
+      countInStock: 10,
+      rating: 4.6,
+      numReviews: 10,
+      desc: 'Skyrim specail edition for steam',
+    },
+  ],
+};
+export default data;
